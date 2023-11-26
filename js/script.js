40d57a1857491fbe4178c0eaa111d473d06d74d4
Tylery77 <141975360+Tylery77@users.noreply.github.com>
2023-11-25 22:28:50 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new fullpage('#fullpage', {
+        //licenseKey: 'gplv3-license',
+        anchors: ['home', 'about', 'portfolio', 'resume', 'contact'],
+        menu: '#menu',
+        afterLoad: function (origin, destination, direction) {
+            const activeMenuItem = document.querySelector('.menu-items a.active');
+            if (activeMenuItem) {
+                activeMenuItem.classList.remove('active');
+            }
+            const newActiveMenuItem = document.querySelector(`.menu-items a[href="#${destination.anchor}"]`);
+            if (newActiveMenuItem) {
+                newActiveMenuItem.classList.add('active');
+            }
+        }
+    });
+
+    // Handle click events on menu items
+    const menuItems = document.querySelectorAll('.menu-items a');
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const activeMenuItem = document.querySelector('.menu-items a.active');
+            if (activeMenuItem) {
+                activeMenuItem.classList.remove('active');
+            }
+            item.classList.add('active');
+        });
+    });
+});
