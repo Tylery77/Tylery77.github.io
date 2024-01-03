d3799e85d75f4f051172e5d11b6f79a0da8be558
Tylery77 <141975360+Tylery77@users.noreply.github.com>
2024-01-03 15:10:50 -0500

    undo changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     new fullpage('#fullpage', {
-        // other options
-        anchors: ['home', 'about', 'portfolio', 'contact'], // Ensure anchors match your sections
-        scrollOverflow: true, // Enable scroll overflow for sections with more content
-        scrollOverflowOptions: {
-            scrollbars: true, // Show scrollbars when content overflows
-        },
-        autoScrolling: true, // Enable automatic scrolling
+        //licenseKey: 'gplv3-license',
+        anchors: ['home', 'about', 'portfolio', 'contact', 'resume' ],
+        menu: '#menu',
         afterLoad: function (origin, destination, direction) {
-            // Add any custom code or checks after each section is loaded
-        },
+            const activeMenuItem = document.querySelector('.menu-items a.active');
+            if (activeMenuItem) {
+                activeMenuItem.classList.remove('active');
+            }
+            const newActiveMenuItem = document.querySelector(`.menu-items a[href="#${destination.anchor}"]`);
+            if (newActiveMenuItem) {
+                newActiveMenuItem.classList.add('active');
+            }
+        }
     });
-    
 
     // Handle click events on menu items
     const menuItems = document.querySelectorAll('.menu-items a');
