d7a711d8302174f1610536ba392d5189a71ed309
Tylery77 <141975360+Tylery77@users.noreply.github.com>
2024-01-03 14:47:14 -0500

    scroll change v1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     new fullpage('#fullpage', {
-        //licenseKey: 'gplv3-license',
-        anchors: ['home', 'about', 'portfolio', 'resume', 'contact'],
-        menu: '#menu',
+        // other options
+        anchors: ['home', 'about', 'portfolio', 'contact'], // Ensure anchors match your sections
+        scrollOverflow: true, // Enable scroll overflow for sections with more content
+        scrollOverflowOptions: {
+            scrollbars: true, // Show scrollbars when content overflows
+        },
+        autoScrolling: true, // Enable automatic scrolling
         afterLoad: function (origin, destination, direction) {
-            const activeMenuItem = document.querySelector('.menu-items a.active');
-            if (activeMenuItem) {
-                activeMenuItem.classList.remove('active');
-            }
-            const newActiveMenuItem = document.querySelector(`.menu-items a[href="#${destination.anchor}"]`);
-            if (newActiveMenuItem) {
-                newActiveMenuItem.classList.add('active');
-            }
-        }
+            // Add any custom code or checks after each section is loaded
+        },
     });
+    
 
     // Handle click events on menu items
     const menuItems = document.querySelectorAll('.menu-items a');
